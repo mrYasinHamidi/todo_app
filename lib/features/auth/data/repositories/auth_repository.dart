f8b824c95e50855e0b54b78b30618c0fc57d6ae0
yas <yas@gmail.com>
2025-05-05 12:23:11 +0330

    add auth repository

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:todo_app/core/error/error.dart';
+import 'package:todo_app/core/repository/base_repository.dart';
+import 'package:todo_app/features/auth/data/services/auth_local_service.dart';
+import 'package:todo_app/features/auth/data/services/auth_remote_service.dart';
+import 'package:todo_app/models/app_user.dart';
+
+class AuthRepository extends BaseRepository {
+  final AuthLocalService _localService;
+  final AuthRemoteService _remoteService;
+
+  const AuthRepository({required AuthLocalService localService, required AuthRemoteService remoteService})
+    : _localService = localService,
+      _remoteService = remoteService;
+
+  Future<Either<Failure, AppUser>> signIn(String email, String password) async {
+    return perform(() async {
+      final remoteUser = await _remoteService.signIn(email, password);
+      final appUser = AppUser(uid: remoteUser!.uid, email: email);
+      await _localService.saveUser(appUser);
+      return appUser;
+    });
+  }
+
+  Future<Either<Failure, AppUser>> signup(String email, String password) async {
+    return perform(() async {
+      final remoteUser = await _remoteService.signUp(email, password);
+      final appUser = AppUser(uid: remoteUser!.uid, email: email);
+      await _localService.saveUser(appUser);
+      return appUser;
+    });
+  }
+
+  Future<Either<Failure, void>> logout() {
+    return perform(() async {
+      await _remoteService.signOut();
+      await _localService.clear();
+    });
+  }
+}
